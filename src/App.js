75475212188b6f4fa550fca3c455ff4a15bfb2dc
prabinjackz <prabinav0@gmail.com>
2022-07-01 16:45:56 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test by prabin
+          Test by prabin - apple boy working correctly
         </p>
         <a
           className="App-link"
